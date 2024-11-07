9dcbe71e59992e062725bbca54b4bab406fb5610
Necromancy501 <94140246+Necromancy501@users.noreply.github.com>
2024-11-08 00:46:25 +0800

    Finish program
    Add several different functions and ensured the program ran as specified with some
    personal additions. The user can specify number of rounds and the program can check
    for several edge cases on the inputs.

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
+// AUXILIAR FUNCTIONS
+
 // Capitalizes a word. Used in "getHumanChoice()"
 
 function capitalize(word) {
   word = word.toLowerCase();
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
+
+// Checks if an input is a valid number (disalow 0). Used in roundsNumber
+
+function isRoundNumber(str){
+
+  // Eliminates surrounding whitespaces
+
+  str = str.trim();
+
+  // If empty string return false
+
+  if (!str){
+    return false;
+  }
+  
+  else {
+
+    // Trims leading zeros
+
+    str = str.replace(/^0+/, '');
+
+    // Forces an integer and stores in number
+
+    number = parseInt(str);
+
+    // Returns number if it's greater than 0, not a string and not infinity. Otherwise returns false
+
+    if ((number !== Infinity) && (String(number) === str) && (number>0)){
+      return number
+    }
+    else {
+      return false;
+    }
+
+  }
+
+}
+
+// MAIN FUNCTIONS
 
 // Returns "rock", "paper" or "scissors" randomly (CPU choice)
 
@@ -23,7 +64,7 @@ function getComputerChoice() {
     options[a] > options[b] ? a : b
   );
 
-  return computerChoice;
+  return capitalize(computerChoice);
 }
 
 // Gets and returns human choice for Rock, Paper, Scissors
@@ -48,7 +89,7 @@ function getHumanChoice() {
       return choice;
     }
 
-    // Otherwise return and keeps the loop going
+    // Otherwise asks again for a valid choice
 
     else {
       console.log("please enter a valid choice");
@@ -61,7 +102,7 @@ function getHumanChoice() {
 
 // Game logic for a round of "Rock, Paper, Scissors"
 
-function playRound(humanChoice, computerChoice) {
+function playRound(username, humanChoice, computerChoice) {
 
   // Object that stores which choice beats which
 
@@ -71,10 +112,13 @@ function playRound(humanChoice, computerChoice) {
     rock: "Paper"
   }
 
+  let isRoundPlayed;
+
   // Checks ties
 
   if (humanChoice === computerChoice) {
     console.log("Tie");
+    isRoundPlayed = false;
   }
 
   // Checks human win
@@ -82,6 +126,7 @@ function playRound(humanChoice, computerChoice) {
   else if (getsBeatenBy[computerChoice.toLowerCase()] === humanChoice) {
     console.log("Round won!")
     humanScore++;
+    isRoundPlayed = true;
   }
 
   // Gives point to computer
@@ -89,11 +134,107 @@ function playRound(humanChoice, computerChoice) {
   else {
     console.log("Round lost")
     computerScore++
+    isRoundPlayed = true;
   }
 
-  // Prints current results and ends the round
+  // Prints current results
 
-  console.log(`You [${humanScore}] - Computer [${computerScore}]`);
+  console.log(`${username} [${humanScore}] - Computer [${computerScore}]`);
+
+  return isRoundPlayed;
+
+}
+
+// Gets number of rounds from user
+
+function roundsNumber(){
+
+  // Gets number of rounds from user
+
+  let rounds = prompt("How many rounds would you like to play?")
+
+  keepGoing = true;
+  
+  while(keepGoing){
+
+    // Checks and assigns rounds number to a valid format
+
+    rounds = isRoundNumber(rounds);
+
+    if(rounds){
+
+      // Returns the number if it's a valid number
+
+      return rounds;
+
+    }
+
+    // Repeats the prompt if necessary
+    
+    console.log("Invalid number of rounds")
+    rounds = prompt("Please enter a valid number of rounds");
+  }
+
+}
+
+// Gets the winner for a full game of "Rock, Paper, Scissors"
+
+function getsWinner(player1, pointsP1, player2, pointsP2){
+
+  // Checks tie, returns "tie"
+
+  if (pointsP1 === pointsP2){
+    return("tie");
+  }
+
+  // Returns winner
+
+  else {
+    return (pointsP1 > pointsP2) ? player1 : player2;
+  }
+
+}
+
+function gameWrapper(){
+
+  // Asks for username
+
+  const username = prompt("Please enter your username");
+
+  // Asks how many rounds to play
+
+  let numberOfRounds = roundsNumber();
+
+  // Plays for a determined number of rounds
+
+  while(numberOfRounds){
+
+    // Plays a round of Rock, Paper, Scissors
+
+    if(playRound(username, getHumanChoice(), getComputerChoice())){
+      
+      // Decrease the number of rounds remaining
+    
+      numberOfRounds--;
+    }
+
+    console.log(`Rounds remaining: ${numberOfRounds}`);
+  }
+
+  // Gets winner of the whole game or "tie"
+
+  const winner = getsWinner(username, humanScore, "Computer", computerScore);
+
+  // Final output messages
+
+  console.log("GAME OVER");
+
+  if(winner==="tie"){
+    console.log("Game concluded in a tie");
+  }
+  else {
+    console.log(`Winner: ${winner}`);
+  }
 
 }
 
@@ -102,6 +243,4 @@ function playRound(humanChoice, computerChoice) {
 let humanScore = 0;
 let computerScore = 0;
 
-// Plays a round of Rock, Paper, Scissors
-
-playRound( getHumanChoice(), getComputerChoice() )
+gameWrapper();
